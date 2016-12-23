43925d11df58909a6d20663fd3b6cb1069f4cd48
Robbie Carlton <robbie.carlton@gmail.com>
2016-12-23 11:49:22 -0700

    group user

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var djb2Hash = function djb2Hash(str) {
+  var hash = 5381;
+  for (var i = 0; i < str.length; i++) {
+    var char = str.charCodeAt(i);
+    hash = (hash << 5) + hash + char;
+  }
+  return hash;
+};
+
+// returns 0 or 1, grouping users into unique groups.
+// Example usage:
+//  if (groupUser(user.id, 'Popular Skill Engagement Module') === 1) {
+//    ... display module ...
+// }
+var groupUser = exports.groupUser = function groupUser(id, groupName) {
+  return Math.abs(id * djb2Hash(groupName) >> 3) % 2;
+};
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const djb2Hash = str => {
+  var hash = 5381
+  for (var i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i)
+    hash = ((hash << 5) + hash) + char
+  }
+  return hash
+}
+
+// returns 0 or 1, grouping users into unique groups.
+// Example usage:
+//  if (groupUser(user.id, 'Popular Skill Engagement Module') === 1) {
+//    ... display module ...
+// }
+export const groupUser = (id, groupName) =>
+  Math.abs(((id * djb2Hash(groupName)) >> 3)) % 2
