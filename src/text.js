0647234bbe84a2345f8579ef81d96c4bce143f86
Lawrence Wang <l@lw.io>
2017-01-19 16:10:05 -0800

    add text/increment and bump to 1.0.2

--- src/text.js
+++ src/text.js
@@ -16,6 +16,15 @@ export function sanitize (text) {
 }
 
 export const markdown = text => {
-  marked.setOptions({gfm: true, breaks: true, sanitize: false})
+  marked.setOptions({gfm: true, breaks: true})
   return marked(text || '')
+}
+
+// increment the number at the end of a string.
+// foo => foo2, foo2 => foo3, etc.
+export const increment = text => {
+  const regex = /\d*$/
+  const word = text.replace(regex, '')
+  const number = Number(text.match(regex)[0] || 1) + 1
+  return `${word}${number}`
 }
--- text.js
+++ text.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.markdown = undefined;
+exports.incrementLabel = exports.markdown = undefined;
 exports.sanitize = sanitize;
 
 var _marked = require('marked');
@@ -31,6 +31,15 @@ function sanitize(text) {
 }
 
 var markdown = exports.markdown = function markdown(text) {
-  _marked2.default.setOptions({ gfm: true, breaks: true, sanitize: false });
+  _marked2.default.setOptions({ gfm: true, breaks: true });
   return (0, _marked2.default)(text || '');
+};
+
+// increment the number at the end of a string.
+// foo => foo2, foo2 => foo3, etc.
+var incrementLabel = exports.incrementLabel = function incrementLabel(label) {
+  var regex = /\d*$/;
+  var word = label.replace(regex, '');
+  var number = Number(label.match(regex)[0] || 1) + 1;
+  return '' + word + number;
 };
