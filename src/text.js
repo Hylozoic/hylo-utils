29b2dea68f7dbf17d90d05611385f6c07969c329
Rich Churcher <rich.churcher@gmail.com>
2017-08-02 17:35:08 +1200

    Reformat to allow double duty in mobile client

--- src/text.js
+++ src/text.js
@@ -92,16 +92,8 @@ export function humanDate (date, short) {
   return ret
 }
 
+// Assumes current user has already been filtered from `names`
 export function threadNames (names) {
-  let nameString = ''
-  switch (names.length) {
-    case 1:
-    case 2:
-      nameString = names.join(', ')
-      break
-    default:
-      nameString = `${names.slice(0, 1).join(', ')} and ${names.length - 1} other${names.length > 2 ? 's' : ''}`
-      break
-  }
-  return nameString
+  if (names.length < 3) return names.join(' & ')
+  return `${names[0]} & ${names.length - 1} other${names.length > 2 ? 's' : ''}`
 }
