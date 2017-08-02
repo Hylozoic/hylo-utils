032f60b837615ca1fbdcbfd42883ec91384ce43e
Rich Churcher <rich.churcher@gmail.com>
2017-08-02 17:46:58 +1200

    Add some simple tests

---
+++ __tests__/text.test.js
@@ -0,0 +1,31 @@
+import * as text from '../src/text.js'
+
+describe('threadNames', () => {
+  it('handles single name correctly', () => {
+    const expected = 'Constantine'
+    const actual = text.threadNames([ 'Constantine' ])
+    expect(actual).toBe(expected)
+  })
+
+  it('handles two names correctly', () => {
+    const expected = 'Constantine & Bartholemew'
+    const actual = text.threadNames([ 'Constantine', 'Bartholemew' ])
+    expect(actual).toBe(expected)
+  })
+
+  it('handles nine names correctly', () => {
+    const expected = 'Constantine & 8 others'
+    const actual = text.threadNames([
+      'Constantine',
+      'Bartholemew',
+      'Gertrude',
+      'Hortense',
+      'Abner',
+      'Zachariah',
+      'Philomena',
+      'Zebediah',
+      'Augustus'
+    ])
+    expect(actual).toBe(expected)
+  })
+})
