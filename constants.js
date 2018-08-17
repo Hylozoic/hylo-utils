a1f23dbfbf0ceba636cf68d3d30f0161ecbc4706
Loren Johnson <loren@hellovenado.com>
2018-08-17 06:52:19 -0700

    Add analytics event constants

--- constants.js
+++ constants.js
@@ -10,3 +10,14 @@ Object.defineProperty(exports, "__esModule", {
 // in hylo-evo. Ask Loren :)
 var MENTION_ENTITY_TYPE = exports.MENTION_ENTITY_TYPE = 'mention';
 var TOPIC_ENTITY_TYPE = exports.TOPIC_ENTITY_TYPE = '#mention';
+
+var AnalyticsEvents = exports.AnalyticsEvents = {
+  COMMENT_CREATED: 'Comment Created',
+  COMMUNITY_CREATED: 'Community Created',
+  COMMUNITY_INVITATION_ACCEPTED: 'Community Invitation Accepted',
+  DIRECT_MESSAGE_SENT: 'Direct Message Sent',
+  POST_CREATED: 'Post Created',
+  POST_UPDATED: 'Post Updated',
+  TOPIC_CREATED: 'Topic Created',
+  VOTED_ON_POST: 'Voted on Post'
+};
